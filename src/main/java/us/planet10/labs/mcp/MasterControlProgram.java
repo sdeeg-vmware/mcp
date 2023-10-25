9baa74eb9e608b98058014950f24614b9c65ec0a
sdeeg-vmware <sdeeg@vmware.com>
2023-10-25 15:19:46 -0700

    basic boot app

---
+++ src/main/java/us/planet10/labs/mcp/MasterControlProgram.java
@@ -0,0 +1,13 @@
+package us.planet10.labs.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterControlProgram {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterControlProgram.class, args);
+	}
+
+}
---
+++ src/test/java/us/planet10/labs/mcp/MCPApplicationTests.java
@@ -0,0 +1,13 @@
+package us.planet10.labs.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MCPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
